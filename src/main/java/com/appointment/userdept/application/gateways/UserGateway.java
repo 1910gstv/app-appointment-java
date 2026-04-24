7e48300ac54ceb2ca5be9102d89323acfa535f20
1910gstv <gustavo.hpaula@outlook.com>
2026-04-23 21:51:48 -0300

    Criando edição de usuário

--- src/main/java/com/appointment/userdept/application/gateways/UserGateway.java
+++ src/main/java/com/appointment/userdept/application/gateways/UserGateway.java
@@ -1,14 +1,15 @@
 package com.appointment.userdept.application.gateways;
 
 import com.appointment.userdept.domain.entity.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface UserGateway {
     User createUser(User user);
     List<User> getAllUsers();
-    UserDetails findByEmail(String email);
-    User editUser(String userId, Map<String, Object> newData);
+    User findByEmail(String email);
+    User editUser(Long id, User newData);
+    Optional findById(Long id);
 }
