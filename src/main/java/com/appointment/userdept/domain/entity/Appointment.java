b13bcb32a6014689da08d4b1402390f82fb383b2
1910gstv <gustavo.hpaula@outlook.com>
2026-04-23 22:18:18 -0300

    criando os records de consultas

--- src/main/java/com/appointment/userdept/domain/entity/Appointment.java
+++ src/main/java/com/appointment/userdept/domain/entity/Appointment.java
@@ -1,4 +1,15 @@
 package com.appointment.userdept.domain.entity;
 
-public record Appointment() {
-}
+import java.time.LocalDateTime;
+
+public record Appointment(
+        Long id,
+        Long userId,
+        LocalDateTime dateTime,
+        Boolean isActive,
+        String description,
+        AppointmentType type,
+        Long providerId,
+        Location location,
+        HealthInsurance healthInsurance
+){};
